0e658e29f68a1ae6802652e2caadfe1ba195aa6b
Alexey <forsage@list.ru>
2022-08-07 10:34:59 +0300

    Задача 3 работает

---
+++ C_sharp_sem/Primer_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу вычисления модуля числа.
+
+int Prompt(string message)
+{
+    	System.Console.Write(message);          // Вывести сообщение
+    	string readValue = Console.ReadLine();  // Считавает с консоли строку
+    	int result = int.Parse(readValue);      // Преобразует строку в целое число
+    	return result;                          // Возвращает результат
+}
+
+int value = Prompt ("Введите число ");
+int newValue = value;
+if (value < 0)
+{
+    newValue = -value;
+};
+
+System.Console.WriteLine($"{newValue}");
