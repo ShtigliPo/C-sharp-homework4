0ee7f390a4120cba0231b04451a6d300e72aaeca
Алексей Зверев <109279176+ShtigliPo@users.noreply.github.com>
2022-08-20 23:07:07 +0300

    Delete C_sharp_hw2 directory

--- C_sharp_hw2/Primer2/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
-
-int num = new Random().Next(100, 1000);
-System.Console.WriteLine(num);
-int num1 = 10 * ( num / 100 )  + ( num % 10 );
-System.Console.WriteLine(num1);
